88355319f93628111b04e4f75ee68c08853d6a2e
Simon Vrachliotis <simon@simonswiss.com>
2016-02-03 15:53:12 +1100

    working on components

--- src/components/OrderForm.js
+++ src/components/OrderForm.js
@@ -1,39 +1,44 @@
 import React, { Component } from 'react'
 
 
-const RadioInput = ({key, value}) => (
-  <label 
-    key={key} 
-    className="radio-inline"
-  >
-    <input 
-      type="radio" 
-      name="coffee" 
-      value={value} 
-    /> 
-    {value}
-  </label>
-)
+class RadioInput extends Component {
+  render() {
+    const { value, name, key } = this.props
+    return (
+      <label 
+        key={key} 
+        className="radio-inline"
+      >
+        <input 
+          type="radio"
+          name={name}
+          value={value} 
+        /> 
+        {value}
+      </label>
+    )
+  }
+}
 
 export default class OrderForm extends Component {
   render() {
     let { menu } = this.props.value
     
     let coffeeTypes = menu.types.map((coffee, key) =>
-      <RadioInput key={key} value={coffee} />
+      <RadioInput name="coffee" ref="coffee" key={key} value={coffee} />
     )
 
-    let coffeeSizes = menu.sizes.map((coffee, key) =>
-      <RadioInput key={key} value={coffee} />
+    let coffeeSizes = menu.sizes.map((size, key) =>
+      <RadioInput name="size" ref="size" key={key} value={size} />
     )
 
-    let coffeeSugar = menu.sugar.map((coffee, key) =>
-      <RadioInput key={key} value={coffee} />
+    let coffeeSugar = menu.sugar.map((sugar, key) =>
+      <RadioInput name="sugar" ref="sugar" key={key} value={sugar} />
     )
 
     return (
       <div className="container">
-        <form action="">
+        <form onSubmit={this.placeOrder.bind(this)} action="">
           <div className="form-group">
             <label>Your name</label>
             <input type="text" ref="name" className="form-control" placeholder="Johny Sleepyhead" />
@@ -51,7 +56,9 @@ export default class OrderForm extends Component {
             {coffeeSugar}
           </div>
 
-          <button type="submit" onClick={this.placeOrder.bind(this)}>Place Order</button>
+          <button 
+            className="btn btn-success"
+            type="submit" onClick={this.placeOrder.bind(this)}>Place Order</button>
         </form>
       </div>
     )
@@ -60,11 +67,17 @@ export default class OrderForm extends Component {
     e.preventDefault()
     let data = {
       id: this.props.value.currentOrder.length,
-      name: 'Hello',
-      type: 'latte',
-      size: 'large',
+      name: this.refs.name.value,
+      type: 'Latte',
+      size: 'Large',
       sugar: 1
+      // name: this.refs.name.value,
+      // type: this.refs.coffee.value,
+      // size: this.refs.size.value,
+      // sugar: this.refs.sugar.value
     }
+
+    console.log(data)
     this.props.addOrder(data)
   }
 }
--- src/components/OrdersList.js
+++ src/components/OrdersList.js
@@ -12,26 +12,29 @@ export default class OrdersList extends Component {
           <td>{sugar}</td>
           <td>
             <button 
+              className="btn btn-danger btn-xs"
               onClick={this.deleteOrder.bind(this, id)}>Delete</button>
           </td>
         </tr>
       )
     })
     return (
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Coffee</th>
-            <th>Size</th>
-            <th>Sugar?</th>
-            <th>Delete</th>
-          </tr>
-        </thead>
-        <tbody>
-          {ordersList}
-        </tbody>
-      </table>
+      <div className="container">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Coffee</th>
+              <th>Size</th>
+              <th>Sugar?</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {ordersList}
+          </tbody>
+        </table>
+      </div>
     )
   }
   deleteOrder(id) {
